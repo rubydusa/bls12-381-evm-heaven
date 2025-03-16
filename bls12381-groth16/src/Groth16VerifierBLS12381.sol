15b4e4558a99c302230cdff8a8fdd8afef2cb0a3
rubydusa <rubydusa@protonmail.com>
2025-03-16 21:00:09 +0200

    cleanup bls12381-groth16

--- bls12381-groth16/src/Groth16VerifierBLS12381.sol
+++ bls12381-groth16/src/Groth16VerifierBLS12381.sol
@@ -1,4 +1,3 @@
-// TODO: convert to template
 // SPDX-License-Identifier: GPL-3.0
 //----------------------------------------------------------------------------//
 //                                                                            //
@@ -27,8 +26,6 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
-// I'm tormeneted, when EOF already :((
-// no idea what's the preferable way to represent verification key and proof data
 // TODO: instead of returning nothing, return error selectors
 contract Groth16Verifier {
     // precompiles
@@ -38,44 +35,47 @@ contract Groth16Verifier {
     uint256 constant q = 0x73eda753299d7d483339d80809a1d80553bda402fffe5bfeffffffff00000001;
 
     // Verification Key data
-    bytes32 constant alphax_0  = 0x0;
-    bytes32 constant alphax_1  = 0x0;
-    bytes32 constant alphay_0  = 0x0;
-    bytes32 constant alphay_1  = 0x0;
-    bytes32 constant betax1_0  = 0x0;
-    bytes32 constant betax1_1  = 0x0;
-    bytes32 constant betax2_0  = 0x0;
-    bytes32 constant betax2_1  = 0x0;
-    bytes32 constant betay1_0  = 0x0;
-    bytes32 constant betay1_1  = 0x0;
-    bytes32 constant betay2_0  = 0x0;
-    bytes32 constant betay2_1  = 0x0;
-    bytes32 constant gammax1_0 = 0x0;
-    bytes32 constant gammax1_1 = 0x0;
-    bytes32 constant gammax2_0 = 0x0;
-    bytes32 constant gammax2_1 = 0x0;
-    bytes32 constant gammay1_0 = 0x0;
-    bytes32 constant gammay1_1 = 0x0;
-    bytes32 constant gammay2_0 = 0x0;
-    bytes32 constant gammay2_1 = 0x0;
-    bytes32 constant deltax1_0 = 0x0;
-    bytes32 constant deltax1_1 = 0x0;
-    bytes32 constant deltax2_0 = 0x0;
-    bytes32 constant deltax2_1 = 0x0;
-    bytes32 constant deltay1_0 = 0x0;
-    bytes32 constant deltay1_1 = 0x0;
-    bytes32 constant deltay2_0 = 0x0;
-    bytes32 constant deltay2_1 = 0x0;
+    bytes32 constant alphax_0  = 0x000000000000000000000000000000000f58323a93c517afe0dbb93b5b3db309;
+    bytes32 constant alphax_1  = 0xb9b641b7056fd14f87987293fe2b7c7671aed23638bfacfd41bd49d190a068ef;
+    bytes32 constant alphay_0  = 0x0000000000000000000000000000000003f84652dc9a9cccf9abe2a577b8884d;
+    bytes32 constant alphay_1  = 0x2b0bab9cbe075633630f0ff8f763e270ac32479b0967af026e89e71e25e9a882;
+    
+    bytes32 constant betax1_0  = 0x000000000000000000000000000000000a2dc4ec42e832824d068834086d1692;
+    bytes32 constant betax1_1  = 0x912e647dd0d82595a30deff528f98c17a59802fe2ff88593920f298b0eb1490b;
+    bytes32 constant betay1_0  = 0x00000000000000000000000000000000056c76173acbcaa2e5d0cdfe1eee5b23;
+    bytes32 constant betay1_1  = 0xd8f169dda6f9a426be2ca041af54ea397dc1b74529e90f6c79141c3288fd9bf2;
+    bytes32 constant betax2_0  = 0x0000000000000000000000000000000008456842198fe0a5544d10fa2247c8db;
+    bytes32 constant betax2_1  = 0xc7c5722722981e74f146860957d161903f06f6ef4e60cb90983f37ba4f888a15;
+    bytes32 constant betay2_0  = 0x000000000000000000000000000000000dadf33cd97b70cf9db1594d3892c705;
+    bytes32 constant betay2_1  = 0x26c6640df2fd4f227e21bb56c851779279b79b11993cb8d2f16cf074de56deed;
+
+    bytes32 constant gammax1_0 = 0x00000000000000000000000000000000024aa2b2f08f0a91260805272dc51051;
+    bytes32 constant gammax1_1 = 0xc6e47ad4fa403b02b4510b647ae3d1770bac0326a805bbefd48056c8c121bdb8;
+    bytes32 constant gammay1_0 = 0x0000000000000000000000000000000013e02b6052719f607dacd3a088274f65;
+    bytes32 constant gammay1_1 = 0x596bd0d09920b61ab5da61bbdc7f5049334cf11213945d57e5ac7d055d042b7e;
+    bytes32 constant gammax2_0 = 0x000000000000000000000000000000000ce5d527727d6e118cc9cdc6da2e351a;
+    bytes32 constant gammax2_1 = 0xadfd9baa8cbdd3a76d429a695160d12c923ac9cc3baca289e193548608b82801;
+    bytes32 constant gammay2_0 = 0x000000000000000000000000000000000606c4a02ea734cc32acd2b02bc28b99;
+    bytes32 constant gammay2_1 = 0xcb3e287e85a763af267492ab572e99ab3f370d275cec1da1aaa9075ff05f79be;
+
+    bytes32 constant deltax1_0 = 0x00000000000000000000000000000000024aa2b2f08f0a91260805272dc51051;
+    bytes32 constant deltax1_1 = 0xc6e47ad4fa403b02b4510b647ae3d1770bac0326a805bbefd48056c8c121bdb8;
+    bytes32 constant deltay1_0 = 0x0000000000000000000000000000000013e02b6052719f607dacd3a088274f65;
+    bytes32 constant deltay1_1 = 0x596bd0d09920b61ab5da61bbdc7f5049334cf11213945d57e5ac7d055d042b7e;
+    bytes32 constant deltax2_0 = 0x000000000000000000000000000000000ce5d527727d6e118cc9cdc6da2e351a;
+    bytes32 constant deltax2_1 = 0xadfd9baa8cbdd3a76d429a695160d12c923ac9cc3baca289e193548608b82801;
+    bytes32 constant deltay2_0 = 0x000000000000000000000000000000000606c4a02ea734cc32acd2b02bc28b99;
+    bytes32 constant deltay2_1 = 0xcb3e287e85a763af267492ab572e99ab3f370d275cec1da1aaa9075ff05f79be;
 
     uint256 constant nPublic = 2;
-    bytes32 constant IC0x_0 = 0x0;
-    bytes32 constant IC0x_1 = 0x0;
-    bytes32 constant IC0y_0 = 0x0;
-    bytes32 constant IC0y_1 = 0x0;
-    bytes32 constant IC1x_0 = 0x0;
-    bytes32 constant IC1x_1 = 0x0;
-    bytes32 constant IC1y_0 = 0x0;
-    bytes32 constant IC1y_1 = 0x0;
+    bytes32 constant IC0x_0 = 0x0000000000000000000000000000000005107a2acbd409bb09513b7a83d0c6b1;
+    bytes32 constant IC0x_1 = 0xc01d528f07c55c5a47b276b3c6df0b5e812927fcb01e7fd3e9a9663b68f3c7d0;
+    bytes32 constant IC0y_0 = 0x000000000000000000000000000000000c5beb467829d8c27eec670976b7d392;
+    bytes32 constant IC0y_1 = 0x43cb8537719120eeb743a962f4f3a752614f372a90bfa8c0e3fdf70fb208c96a;
+    bytes32 constant IC1x_0 = 0x0000000000000000000000000000000001a5ef7fe21f48d5f3677f45de2fdce1;
+    bytes32 constant IC1x_1 = 0xac94bd836f74d82a58efa58908e930fa4dbb77788f03e32da7f3f036b3c76e2a;
+    bytes32 constant IC1y_0 = 0x000000000000000000000000000000000675ad2922bde59f673a244ada6c3846;
+    bytes32 constant IC1y_1 = 0x6de1af493a8ef3dc04fc624f8db59deeff42514577e95dc67e5c526973017bb6;
 
     // proof structure:
     // negA: 128 bytes
@@ -90,7 +90,7 @@ contract Groth16Verifier {
         proofLength = 512 + 32 * nPublic;
     }
  
-    function verifyProof(bytes calldata proof) public view returns (bool isValid){
+    function verifyProof(bytes calldata proof) public view returns (bool){
         // assembly does not support immutable variables
         uint256 _proofLength = proofLength;
         assembly {
@@ -110,16 +110,17 @@ contract Groth16Verifier {
             let o := proof.offset
             // msm of pub signals
             mstore(0, IC0x_0)
-            mstore(32, IC0y_0)
-            mstore(64, IC1x_0)
-            mstore(96, IC1y_0)
+            mstore(32, IC0x_1)
+            mstore(64, IC0y_0)
+            mstore(96, IC0y_1)
             calldatacopy(128, add(o, 512), 32)
-            mstore(160, IC0x_1)
-            mstore(192, IC0y_1)
-            mstore(224, IC1x_1)
+            mstore(160, IC1x_0)
+            mstore(192, IC1x_1)
+            mstore(224, IC1y_0)
             mstore(256, IC1y_1)
             calldatacopy(288, add(o, 544), 32)
             // vk
+            // TODO: optimize by not including IC0 in the MSM (since every point multiplied by 1 is itself)
             let success := staticcall(sub(gas(), 2000), G1_MSM_PRECOMPILE, 0, 320, 0, 128)
             if iszero(success) {
                 mstore(0, 0)
@@ -138,7 +139,7 @@ contract Groth16Verifier {
             mstore(320, gammay2_0)
             mstore(352, gammay2_1)
             // C
-            calldatacopy(384, add(0, 384), 128)
+            calldatacopy(384, add(o, 384), 128)
             // delta
             mstore(512, deltax1_0)
             mstore(544, deltax1_1)
@@ -172,7 +173,7 @@ contract Groth16Verifier {
                 return(0, 0x20)
             }
 
-            isValid := mload(0)
+            return(0, 0x20)
         }
     }
  }
